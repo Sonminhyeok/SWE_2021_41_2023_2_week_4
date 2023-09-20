b0aeba62c9123e6f97a1c7e037ad42c25050a4f1
Sonminhyeok <50442066+Sonminhyeok@users.noreply.github.com>
2023-09-20 19:59:53 +0900

    Update arrange.sh

--- arrange.sh
+++ arrange.sh
@@ -2,7 +2,7 @@
 
 for file in files/*; 
 do
-  fime_name=$(basename "$file")
+  file_name=$(basename "$file")
   name_temp=$(echo "$file_name" | grep -o '^.')
   name_lower=${name_temp,,}
   mv "$file" "$name_lower"/
